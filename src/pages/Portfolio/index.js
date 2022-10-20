c71fa40c102e0ce8d23f8f58bd78b0643d151e9a
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-10-20 00:10:15 -0700

    complete slide layout

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
 // Import Swiper styles
-import 'swiper/css';
-import "swiper/css/effect-coverflow";
+import "swiper/css";
 import "swiper/css/pagination";
+
 // import required modules
-import { EffectCoverflow, Pagination } from "swiper";
+import { Pagination } from "swiper";
 
 import "./portfolio.css";
 
@@ -19,52 +20,28 @@ const PortfolioPage = () => {
                 <div className='carousel-header'>
                     <h1> My Works</h1>
                 </div>
-                <div className='carousel-slide'>
-                    <Swiper
-                        effect={"coverflow"}
-                        grabCursor={true}
-                        centeredSlides={true}
-                        slidesPerView={"auto"}
-                        coverflowEffect={{
-                            rotate: 50,
-                            stretch: 0,
-                            depth: 50,
-                            modifier: 1,
-                            slideShadows: true,
-                        }}
-                        pagination={true}
-                        modules={[EffectCoverflow, Pagination]}
-                        className="mySwiper"
-                    >
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
+               
+                <Swiper
+                    slidesPerView={5}
+                    spaceBetween={20}
+                    grabCursor={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+                
             </div>
         </>
     )
