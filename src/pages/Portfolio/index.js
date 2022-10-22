3f3d4a2c555b05c99c27c584c643aa5ef2eecc88
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-10-21 18:28:32 -0700

    get the slide working

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -5,24 +5,31 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-cube";
 import "swiper/css/pagination";
 
+
 // import required modules
-import { Pagination } from "swiper";
+import { EffectCube, Pagination } from "swiper";
 
 import "./portfolio.css";
+
+
 
 const PortfolioPage = () => {
 
     return (
+
         <>
+
             <div className='carousel-area'>
                 <div className='carousel-header'>
                     <h1> My Works</h1>
                 </div>
-               
+
                 <Swiper
-                    slidesPerView={5}
+                    // direction={"vertical"}
+                    slidesPerView={1}
                     spaceBetween={20}
                     grabCursor={true}
                     pagination={{
@@ -31,8 +38,67 @@ const PortfolioPage = () => {
                     modules={[Pagination]}
                     className="mySwiper"
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>
+
+
+                        <Swiper
+                            effect={"cube"}
+                            grabCursor={true}
+                            cubeEffect={{
+                                shadow: true,
+                                slideShadows: true,
+                                shadowOffset: 180,
+                                shadowScale: 0.94,
+                            }}
+
+                            modules={[EffectCube, Pagination]}
+                            className="myCubeSwiper"
+                        >
+
+                            <SwiperSlide>
+                                <div className='project-content'>Name of the project</div>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/WorkdayScheduler.png" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/WeatherDashboard.png" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/TextEditor.png" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/TechBlog.png" />
+                            </SwiperSlide>
+                        </Swiper>
+
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Swiper
+                            effect={"cube"}
+                            grabCursor={true}
+                            cubeEffect={{
+                                shadow: true,
+                                slideShadows: true,
+                                shadowOffset: 180,
+                                shadowScale: 0.94,
+                            }}
+
+                            modules={[EffectCube, Pagination]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/NoteTaker.png" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/projects/projectImages/CodeQuiz.png" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                            </SwiperSlide>
+                        </Swiper>
+                    </SwiperSlide>
                     <SwiperSlide>Slide 3</SwiperSlide>
                     <SwiperSlide>Slide 4</SwiperSlide>
                     <SwiperSlide>Slide 5</SwiperSlide>
@@ -41,7 +107,6 @@ const PortfolioPage = () => {
                     <SwiperSlide>Slide 8</SwiperSlide>
                     <SwiperSlide>Slide 9</SwiperSlide>
                 </Swiper>
-                
             </div>
         </>
     )
