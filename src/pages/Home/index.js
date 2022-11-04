26ffb2bc95db92262bc12ce5cc6c4fe20d31f16a
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-11-03 23:52:31 -0700

    home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,16 @@ import Image from 'react-bootstrap/Image';
 import "bootstrap/dist/css/bootstrap.css";
 import 'animate.css';
 import './home.css';
+
+import Hoody from "../../assets/images/hoody.png"
+
 const homePage = () => {
     return (
         <>
             <div className="imageDiv">
-                <Image className="animate" src="https://raw.githubusercontent.com/Khanh-T-Tran/myportfolio-react/main/src/assets/images/3hoodies-darktheme.png" fluid />              
+                <Image className="animate" src={Hoody} fluid />              
+                <Image className="animate" src={Hoody} fluid />              
+                <Image className="animate" src={Hoody} fluid />              
             </div>
         </>
     )
